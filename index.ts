f44f3829a33624865d919d5274816f151f49fb08
AbdullahGhani1 <engr.abdullahghani@gmail.com>
2025-09-18 12:46:46 +0500

    Array and Object

--- index.ts
+++ index.ts
@@ -3,4 +3,20 @@ const duckAge: number = 3;
 
 let duckColor: string = 'Black';
 
-console.log(duckAge);
+const ducks: string[] = ['Daffy'];
+
+interface Duck {
+  name: string;
+  age: number;
+  type: string;
+  color: string;
+}
+
+const duck: Duck = {
+  name: 'Daffy',
+  age: 3,
+  type: 'Mallard',
+  color: 'Black',
+};
+
+console.log(duck.age);
